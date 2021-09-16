d5c5781f4bb93a967937f35c9da5850e88b25aff
Steve Ognibene <steve.ognibene@namely.com>
2021-09-16 09:04:51 -0400

    initial commit

---
+++ example1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
